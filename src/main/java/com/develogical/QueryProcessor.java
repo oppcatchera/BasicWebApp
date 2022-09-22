fbf0ccf2607cc4d3b44e1fac7f8564c235b9fd6b
oppcatchera <65623690+oppcatchera@users.noreply.github.com>
2022-09-22 01:49:45 -0400

    minor changes

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("17313")) {
+            return "FCE hours / week should be 80";
+        }
         return "";
     }
 }
